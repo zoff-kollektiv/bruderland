c776db256b9be1d7e9876c3668b64b5e99672af7
Gustav Pursche <pursche@posteo.de>
2018-10-29 15:54:53 +0100

    Move src/static to /static

--- src/components/with-layout/styles.js
+++ src/components/with-layout/styles.js
@@ -17,8 +17,8 @@ export default css`
     font-family: 'Lapture';
     font-style: italic;
     font-weight: 700;
-    src: url('/static/lapture/lapture-bold-italic.woff2') format('woff2'),
-      url('/static/lapture/lapture-bold-italic.woff') format('woff');
+    src: url('/fonts/lapture/lapture-bold-italic.woff2') format('woff2'),
+      url('/fonts/lapture/lapture-bold-italic.woff') format('woff');
   }
 
   @font-face {
@@ -26,8 +26,8 @@ export default css`
     font-family: 'Superla';
     font-style: normal;
     font-weight: 400;
-    src: url('/static/superla/superla-book.woff2') format('woff2'),
-      url('/static/superla/superla-book.woff') format('woff');
+    src: url('/fonts/superla/superla-book.woff2') format('woff2'),
+      url('/fonts/superla/superla-book.woff') format('woff');
   }
 
   @font-face {
@@ -35,7 +35,7 @@ export default css`
     font-family: 'Superla';
     font-style: normal;
     font-weight: 900;
-    src: url('/static/superla/superla-extra-bold.woff2') format('woff2'),
-      url('/static/superla/superla-extra-bold.woff') format('woff');
+    src: url('/fonts/superla/superla-extra-bold.woff2') format('woff2'),
+      url('/fonts/superla/superla-extra-bold.woff') format('woff');
   }
 `;

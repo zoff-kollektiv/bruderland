3f344d2dc47705197555ccf95fea44b00e2edc6e
Gustav Pursche <pursche@posteo.de>
2018-10-29 15:24:32 +0100

    Add react-helmet

--- src/templates/episode.jsx
+++ src/templates/episode.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import {Helmet} from 'react-helmet';
+import React, { Fragment } from 'react';
 import { graphql } from 'gatsby';
 
 import Episode from '../components/episode';
@@ -7,8 +8,16 @@ export default ({ data }) => {
   const { allWordpressWpEpisodes } = data;
   const { edges } = allWordpressWpEpisodes;
   const { node } = edges[0];
+  const { title } = node;
 
-  return <Episode data={node} />;
+  return <Fragment>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>{title}</title>
+    </Helmet>
+
+    <Episode data={node} />;
+  </Fragment>
 };
 
 export const query = graphql`

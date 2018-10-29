c29008d859719732efeb82368884914284ee35c1
Gustav Pursche <pursche@posteo.de>
2018-10-29 16:12:37 +0100

    Richtext: enhance styles

--- src/components/blocks/richtext/styles.js
+++ src/components/blocks/richtext/styles.js
@@ -7,7 +7,7 @@ export default css`
     align-items: center;
     display: flex;
     flex-direction: column;
-    max-width: 400px;
+    justify-content: center;
   }
 
   :global(.richtext p) {
@@ -15,6 +15,19 @@ export default css`
     font-size: 1.25rem;
     font-weight: ${fonts.superla.weight.regular};
     line-height: 1.4;
-    margin-left: 4rem;
+    max-width: 700px;
+  }
+
+  :global(.richtext h2) {
+    font-family: ${fonts.lapture.family};
+    font-size: 2.875rem;
+    font-style: italic;
+    font-weight: ${fonts.lapture.weight.bold};
+    line-height: 1.1;
+    margin-bottom: 2rem;
+    margin-left: -3rem;
+    margin-top: 4.5rem;
+    max-width: 700px;
+    width: 100%;
   }
 `;

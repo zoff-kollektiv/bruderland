1925fde277c91763b70c6deea725d07c7c2555df
Gustav Pursche <pursche@posteo.de>
2018-11-08 15:33:55 +0100

    Episode: flatten graphql query

--- src/templates/episode.jsx
+++ src/templates/episode.jsx
@@ -7,9 +7,7 @@ import Navigation from '../components/navigation';
 
 export default ({ data }) => {
   const { episode, allEpisodes } = data;
-  const { edges } = episode;
-  const { node } = edges[0];
-  const { title } = node;
+  const { title } = episode;
 
   return (
     <Fragment>
@@ -18,27 +16,21 @@ export default ({ data }) => {
         <title>{title}</title>
       </Helmet>
       <Navigation items={allEpisodes.edges} />
-      <Episode data={node} />;
+      <Episode data={episode} />;
     </Fragment>
   );
 };
 
 export const query = graphql`
   query($number: String) {
-    episode: allWordpressWpEpisodes(
-      filter: { acf: { number: { eq: $number } } }
-    ) {
-      edges {
-        node {
-          title
-          acf {
-            ...intro
-            content_episodes {
-              ...quote
-              ...richtext
-              ...imageTextCombination
-            }
-          }
+    episode: wordpressWpEpisodes(acf: { number: { eq: $number } }) {
+      title
+      acf {
+        ...intro
+        content_episodes {
+          ...quote
+          ...richtext
+          ...imageTextCombination
         }
       }
     }

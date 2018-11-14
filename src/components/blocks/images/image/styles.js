01bb339bd696fe1e217584d93b0db18c7eecec12
Gustav Pursche <pursche@posteo.de>
2018-11-14 10:26:42 +0100

    Image: add styles for small screens

--- src/components/blocks/images/image/styles.js
+++ src/components/blocks/images/image/styles.js
@@ -1,6 +1,6 @@
 import css from 'styled-jsx/css';
 
-import { fonts } from '../../../../tokens';
+import { fonts, mq } from '../../../../tokens';
 
 export default css`
   figure {
@@ -34,16 +34,31 @@ export default css`
     font-size: 1.125rem;
     font-weight: ${fonts.superla.weight.regular};
     line-height: 1.33;
-    margin-top: 2rem;
+    margin-top: 0;
     max-width: 30rem;
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+    text-align: center;
   }
 
-  .is-fullscreen figcaption {
-    bottom: 2.5rem;
-    color: white;
-    left: 2.5rem;
-    max-width: 15rem;
-    position: absolute;
-    text-shadow: 0 2px 3px 0 rgba(0, 0, 0, 0.5);
+  @media ${mq.tablet} {
+    figcaption {
+      margin-top: 2rem;
+      padding-left: 0.5rem;
+      padding-right: 0.5rem;
+      text-align: left;
+    }
+  }
+
+  @media ${mq.tablet} {
+    .is-fullscreen figcaption {
+      bottom: 2.5rem;
+      color: white;
+      left: 2.5rem;
+      max-width: 15rem;
+      position: absolute;
+      text-align: left;
+      text-shadow: 0 2px 3px 0 rgba(0, 0, 0, 0.5);
+    }
   }
 `;

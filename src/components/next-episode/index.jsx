9a1a89402ff07c54ed9f677aff840b70df0656d9
Gustav Pursche <pursche@posteo.de>
2018-11-14 10:57:21 +0100

    NextEpisode: add arrow down

--- src/components/next-episode/index.jsx
+++ src/components/next-episode/index.jsx
@@ -1,11 +1,19 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import Intro from '../blocks/intro';
 import Share from './share';
 
+import styles, { arrowDownIcon } from './styles';
+
+import ArrowDownIcon from '../../static/long-arrow-down.svg';
+
 export default ({ node: { acf, title, slug } }) => (
-  <Fragment>
+  <footer>
+    <style jsx>{styles}</style>
+    {arrowDownIcon.styles}
+
     <Share />
+
     <Intro
       renderQuote={false}
       linkTitle
@@ -13,5 +21,9 @@ export default ({ node: { acf, title, slug } }) => (
       title={title}
       {...acf}
     />
-  </Fragment>
+
+    <div className="next-icon">
+      <ArrowDownIcon className={arrowDownIcon.className} />
+    </div>
+  </footer>
 );

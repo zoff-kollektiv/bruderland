99a204b5f2f4fbb1f04bd9eb6cb9fcde336690ba
Gustav Pursche <pursche@posteo.de>
2018-11-14 13:22:11 +0100

    Text: add footnotes

---
+++ src/components/blocks/richtext/footnotes/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './styles';
+
+export default ({ id, entries }) => (
+  <ul>
+    <style jsx>{styles}</style>
+
+    {entries.map(({ number, text }) => (
+      <li id={`footnote-${id}-${number}`} className="footnote">
+        ({number}) <span dangerouslySetInnerHTML={{ __html: text }} />
+      </li>
+    ))}
+  </ul>
+);
--- src/components/blocks/richtext/index.jsx
+++ src/components/blocks/richtext/index.jsx
@@ -2,23 +2,52 @@ import { graphql } from 'gatsby';
 import React from 'react';
 
 import Constraint from '../../constraint';
+import Footnotes from './footnotes';
 
 import styles from './styles';
 
-export default ({ text }) => (
+const replaceFootnotes = (id, text) => {
+  let newText = text;
+  const search = /#([0-9]+)/g;
+  const matches = newText.match(search);
+
+  if (matches) {
+    matches.forEach(match => {
+      const number = match.split('#')[1];
+      const link = `<a href="#footnote-${id}-${number}" class="footnote">(${number})</a>`;
+
+      newText = newText.replace(match, link);
+    });
+  }
+
+  return newText;
+};
+
+export default ({ footnotesRepeat, id, text }) => (
   <section>
     <style jsx>{styles}</style>
 
     <div className="richtext">
       <Constraint>
-        <div dangerouslySetInnerHTML={{ __html: text }} />
+        <div dangerouslySetInnerHTML={{ __html: replaceFootnotes(id, text) }} />
       </Constraint>
     </div>
+
+    <Constraint>
+      {footnotesRepeat && footnotesRepeat.length > 0 && (
+        <Footnotes id={id} entries={footnotesRepeat} />
+      )}
+    </Constraint>
   </section>
 );
 
 export const fragment = graphql`
   fragment richtext on WordPressAcf_text {
+    id
     text
+    footnotesRepeat {
+      number
+      text
+    }
   }
 `;

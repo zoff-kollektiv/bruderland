bb3662dc806856a2cb3697a72766888f9da20ec4
Gustav Pursche <pursche@posteo.de>
2018-11-16 14:05:59 +0100

    Episode: remove wrapping <main> element

--- src/components/episode/index.jsx
+++ src/components/episode/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import BlockError from '../block-error';
 import blocks from '../blocks';
@@ -14,10 +14,11 @@ const Episode = ({ data: { acf }, next }) => {
   const { content_episodes: contentBlocks, ...intro } = acf;
 
   return (
-    <main>
+    <Fragment>
       <style jsx>{styles}</style>
 
       <Intro {...intro} />
+
       <main>
         {contentBlocks.map((block, index) => {
           const { __typename, ...rest } = block;
@@ -38,7 +39,7 @@ const Episode = ({ data: { acf }, next }) => {
       </main>
 
       {next && <NextEpisode {...next} />}
-    </main>
+    </Fragment>
   );
 };
 

4bdfd3f1ab022f84ccc60f1aaa3ed5e6eb09482d
Gustav Pursche <pursche@posteo.de>
2019-04-18 12:33:49 +0200

    Intro: allow to skip the whole section

--- src/components/blocks/intro/index.jsx
+++ src/components/blocks/intro/index.jsx
@@ -1,10 +1,16 @@
 import Link from 'gatsby-link';
-import React, { Fragment } from 'react';
+import React, { Fragment, useRef } from 'react';
 
 import ChevronDown from '../../../static/chevron-down.svg';
 import Constraint from '../../constraint';
 import HandshakeIcon from '../../../static/logo-handshake.svg';
 import styles, { nextLink, logoIcon, arrowIcon } from './styles';
+
+const skipIntro = el => {
+  const { offsetHeight } = el;
+
+  window.scrollBy({ top: offsetHeight, left: 0, behavior: 'smooth' });
+};
 
 export default ({
   backgroundImage,
@@ -15,55 +21,62 @@ export default ({
   linkTitle = false,
   linkTitleSlug = '',
   title
-}) => (
-  <header className="js-intro">
-    <style jsx>{styles}</style>
+}) => {
+  const introRef = useRef(null);
 
-    {nextLink.styles}
+  return (
+    <header className="js-intro">
+      <style jsx>{styles}</style>
 
-    {renderQuote && (
-      <div className="quote-container">
-        {backgroundImage.localFile && (
-          <img
-            src={backgroundImage.localFile.childImageSharp.fluid.src}
-            srcSet={backgroundImage.localFile.childImageSharp.fluid.srcSet}
-            alt={backgroundImage.alt_text}
-            className="image"
-          />
-        )}
+      {nextLink.styles}
 
-        <div className="logo">
-          {logoIcon.styles}
-          <HandshakeIcon className={logoIcon.className} />
-        </div>
-
-        <div className="quote">
-          <blockquote>{quote}</blockquote>
-
-          {arrowIcon.styles}
-
-          <ChevronDown className={arrowIcon.className} />
-        </div>
-      </div>
-    )}
-
-    <div className="lower-intro">
-      <Constraint size="wide">
-        <h1 className="title">
-          {linkTitle ? (
-            <Link
-              className={nextLink.className}
-              to={`/episodes/${linkTitleSlug}/`}
-            >
-              {title}
-            </Link>
-          ) : (
-            <Fragment>{title}</Fragment>
+      {renderQuote && (
+        <div className="quote-container" ref={introRef}>
+          {backgroundImage.localFile && (
+            <img
+              src={backgroundImage.localFile.childImageSharp.fluid.src}
+              srcSet={backgroundImage.localFile.childImageSharp.fluid.srcSet}
+              alt={backgroundImage.alt_text}
+              className="image"
+            />
           )}
-        </h1>
 
-        <p className="text">{intro || text}</p>
-      </Constraint>
-    </div>
-  </header>
-);
+          <div className="logo">
+            {logoIcon.styles}
+            <HandshakeIcon className={logoIcon.className} />
+          </div>
+
+          <div className="quote">
+            <blockquote>{quote}</blockquote>
+
+            {arrowIcon.styles}
+
+            <ChevronDown
+              className={arrowIcon.className}
+              onClick={() => skipIntro(introRef.current)}
+            />
+          </div>
+        </div>
+      )}
+
+      <div className="lower-intro">
+        <Constraint size="wide">
+          <h1 className="title">
+            {linkTitle ? (
+              <Link
+                className={nextLink.className}
+                to={`/episodes/${linkTitleSlug}/`}
+              >
+                {title}
+              </Link>
+            ) : (
+              <Fragment>{title}</Fragment>
+            )}
+          </h1>
+
+          <p className="text">{intro || text}</p>
+        </Constraint>
+      </div>
+    </header>
+  );
+};

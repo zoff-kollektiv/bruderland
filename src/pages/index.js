f011b46979a5f7376cc9ad431a8cd82a2cf1c7e4
Gustav Pursche <pursche@posteo.de>
2019-04-21 14:52:04 +0200

    Add proper index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () =>
+  <section style={{ alignItems: 'center', backgroundColor: 'red', display: 'flex', height: '100vh', justifyContent: 'center' }}>
+    <style type="text/css">{`
+      body {
+        margin: 0;
+        padding: 0;
+      }
+    `}</style>
+
+    <h1 style={{ color: 'white', fontFamily: 'Arial', fontWeight: 400, maxWidth: '30rem', padding: '1rem', textAlign: 'center' }}>In Kürze erscheint hier die Webdokumentation "Eigensinn im Bruderland"</h1>
+  </section>

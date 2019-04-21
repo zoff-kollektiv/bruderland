6c89c0f84d41bb0ebfc3787ddda8282c1b38f535
Gustav Pursche <pursche@posteo.de>
2019-04-21 07:42:03 +0200

    Extend default background page template

--- src/templates/background.jsx
+++ src/templates/background.jsx
@@ -1,3 +1,30 @@
+import { Helmet } from 'react-helmet';
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default () => <h1>Background</h1>;
+import Navigation from '../components/navigation';
+import withLayout from '../components/with-layout';
+
+const Page = ({ data: { page } }) => {
+  const { title } = page;
+
+  return (
+    <>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+
+      <Navigation items={[]} topic={title} />
+    </>
+  );
+};
+
+export default withLayout(Page);
+
+export const query = graphql`
+  query($wordpressId: Int) {
+    page: wordpressPage(wordpress_id: { eq: $wordpressId }) {
+      title
+    }
+  }
+`;

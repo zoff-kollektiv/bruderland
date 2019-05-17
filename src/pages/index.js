c87269dea655392dd7204b908e3661f9fe9f742f
Gustav Pursche <pursche@posteo.de>
2019-05-17 20:13:31 +0200

    Update interim page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
 import React from "react"
 
 export default () =>
-  <section style={{ alignItems: 'center', backgroundColor: 'red', display: 'flex', height: '100vh', justifyContent: 'center' }}>
+  <section style={{ alignItems: 'center', backgroundColor: 'red', display: 'flex', flexDirection: 'column', height: '100vh', justifyContent: 'center' }}>
     <style type="text/css">{`
       body {
+        color: white;
+        font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
         margin: 0;
         padding: 0;
       }
     `}</style>
 
-    <h1 style={{ color: 'white', fontFamily: 'Arial', fontWeight: 400, maxWidth: '30rem', padding: '1rem', textAlign: 'center' }}>In Kürze erscheint hier die Webdokumentation "Eigensinn im Bruderland"</h1>
+    <h1 style={{ fontSize: '2rem', fontWeight: 900, maxWidth: '45rem', padding: '0 1rem', textAlign: 'center' }}>In Kürze erscheint hier die Webdokumentation "Eigensinn im Bruderland"</h1>
+    <p style={{ textAlign: 'center', lineHeight: '1.45', maxWidth: '40rem' }}>Ein Projekt des Zentrums für Antisemitismusforschung TU Berlin und der out of focus filmproduktion. Gefördert mit Mitteln der Bundesstiftung zur Aufarbeitungder SED-Diktatur und der Rosa-Luxemburg-Stiftung.</p>
   </section>

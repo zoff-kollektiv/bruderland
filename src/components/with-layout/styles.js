59968efd49c3c1dc037b57f0bd74acc7a2e13e03
Gustav Pursche <pursche@posteo.de>
2019-05-21 15:01:55 +0200

    Improve font rendering across all components

--- src/components/with-layout/styles.js
+++ src/components/with-layout/styles.js
@@ -2,6 +2,9 @@ import css from 'styled-jsx/css';
 
 export default css`
   :global(body) {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
     margin: 0;
     padding: 0;
   }

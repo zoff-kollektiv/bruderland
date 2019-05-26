184ee7dbccc585f03ce18d7ccec03c4b17f0d92a
Gustav Pursche <pursche@posteo.de>
2019-05-26 16:29:42 +0200

    Revert "Add html lang attribute"
    This reverts commit 579fd45514d2e1cf5956bbbee4763d050c98c9fb.

--- src/components/with-layout/index.jsx
+++ src/components/with-layout/index.jsx
@@ -1,4 +1,3 @@
-import Helmet from 'react-helmet';
 import React, { Fragment } from 'react';
 
 import styles from './styles';
@@ -6,8 +5,6 @@ import styles from './styles';
 export default WrappedComponent => props => (
   <Fragment>
     <style jsx>{styles}</style>
-
-    <Helmet htmlAttributes={{ lang: 'de' }} titleTemplate="%s | Bruderland" />
 
     <WrappedComponent {...props} />
   </Fragment>

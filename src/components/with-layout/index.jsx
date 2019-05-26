579fd45514d2e1cf5956bbbee4763d050c98c9fb
Gustav Pursche <pursche@posteo.de>
2019-05-26 16:02:49 +0200

    Add html lang attribute

--- src/components/with-layout/index.jsx
+++ src/components/with-layout/index.jsx
@@ -1,3 +1,4 @@
+import Helmet from 'react-helmet';
 import React, { Fragment } from 'react';
 
 import styles from './styles';
@@ -5,6 +6,8 @@ import styles from './styles';
 export default WrappedComponent => props => (
   <Fragment>
     <style jsx>{styles}</style>
+
+    <Helmet htmlAttributes={{ lang: 'de' }} titleTemplate="%s | Bruderland" />
 
     <WrappedComponent {...props} />
   </Fragment>

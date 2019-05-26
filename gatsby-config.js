6fa73829f1e470129ab2491e3d2280dbd095ac4c
Gustav Pursche <pursche@posteo.de>
2019-05-26 15:25:24 +0200

    Fix preload header

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,12 +27,12 @@ module.exports = {
     },
 
     {
-      resolve: `gatsby-plugin-netlify`,
+      resolve: 'gatsby-plugin-netlify',
       options: {
         allPageHeaders: [
           'Link: </fonts/lapture/lapture-bold-italic.woff2>; as=font; rel=preload',
           'Link: </fonts/superla/superla-book.woff2>; as=font; rel=preload',
-          'Link: <//fonts/superla/superla-extra-bold.woff2>; as=font; rel=preload'
+          'Link: </fonts/superla/superla-extra-bold.woff2>; as=font; rel=preload'
         ]
       }
     },

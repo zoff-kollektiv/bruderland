8df55d12a739c405f1137af62630ee4037e2cb01
Gustav Pursche <pursche@posteo.de>
2019-05-28 18:32:23 +0200

    Metadata: disable iOS phone number detection

--- src/components/metadata/index.jsx
+++ src/components/metadata/index.jsx
@@ -5,11 +5,22 @@ export default ({ title, ogImage, twitterImage, ogTitle, ogDescription }) => (
   <Helmet>
     {title && <title>{title}</title>}
 
+    {/* disable phone number detection on iOS */}
+    <meta name="format-detection" content="telephone=no" />
+
+    {/* Facebook */}
     <meta property="og:type" content="website" />
     <meta property="og:site_name" content="Bruderland" />
 
     <meta property="og:title" content={ogTitle || title} />
-    {ogDescription && <meta property="og:title" content={ogDescription} />}
+
+    {ogDescription && (
+      <>
+        <meta name="description" content={ogDescription} />
+        <meta property="og:title" content={ogDescription} />
+      </>
+    )}
+
     {ogImage && ogImage.localFile && (
       <meta
         property="og:image"
@@ -17,8 +28,10 @@ export default ({ title, ogImage, twitterImage, ogTitle, ogDescription }) => (
       />
     )}
 
+    {/* Twitter */}
     <meta name="twitter:card" content="summary_large_image" />
     <meta property="twitter:title" content={ogTitle || title} />
+
     {ogDescription && (
       <meta property="twitter:description" content={ogDescription} />
     )}

bc02a93b376e9d49a390c8c79bae69f808a955e1
Gustav Pursche <pursche@posteo.de>
2019-06-05 12:55:14 +0200

    Fix tracking base url

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ exports.onClientEntry = () => {
   const elem = document.createElement('div');
 
   elem.id = 'statify-js-snippet';
-  elem.setAttribute('data-home-url', 'https://develop--bruderland.netlify.com');
+  elem.setAttribute('data-home-url', 'https://b2oulrk7.myraidbox.de');
 
   const trackingSnippet =
     'https://b2oulrk7.myraidbox.de/wp-content/plugins/statify/js/snippet.js';

2be9fff6f1dbbba1b787e1b0520cac98d68e3cc1
Gustav Pursche <pursche@posteo.de>
2019-06-05 15:12:03 +0200

    Share: finalize URLs

--- src/components/next-episode/share/index.jsx
+++ src/components/next-episode/share/index.jsx
@@ -8,39 +8,53 @@ import TwitterIcon from '../../../static/twitter.svg';
 
 import styles, { shareIcon } from './styles';
 
-export default ({ title }) => (
-  <footer>
-    <style jsx>{styles}</style>
-    {shareIcon.styles}
+export default ({ title }) => {
+  let currentUrl = '';
+  const sharingText = `${title} | Eigensinn im Bruderland`;
 
-    <Constraint>
-      <h4 className="title">Teile die Episode {title} auf</h4>
+  if (typeof window !== 'undefined') {
+    currentUrl = window.location.href;
+  }
 
-      <ul>
-        <li>
-          <a
-            href="https://facebook.com/"
-            aria-label="Episode auf Facebook teilen"
-          >
-            <FacebookIcon className={shareIcon.className} />
-          </a>
-        </li>
-        <li>
-          <a
-            href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
-              title
-            )}`}
-            aria-label="Episode auf Twitter teilen"
-          >
-            <TwitterIcon className={shareIcon.className} />
-          </a>
-        </li>
-        <li>
-          <a href="mailto:" aria-label="Episode per Email teilen">
-            <EnvelopeIcon className={shareIcon.className} />
-          </a>
-        </li>
-      </ul>
-    </Constraint>
-  </footer>
-);
+  return (
+    <footer>
+      <style jsx>{styles}</style>
+      {shareIcon.styles}
+
+      <Constraint>
+        <h4 className="title">Teile die Episode {title} auf</h4>
+
+        <ul>
+          <li>
+            <a
+              href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+                currentUrl
+              )}`}
+              aria-label="Episode auf Facebook teilen"
+            >
+              <FacebookIcon className={shareIcon.className} />
+            </a>
+          </li>
+          <li>
+            <a
+              href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+                sharingText
+              )}&url=${encodeURIComponent(currentUrl)}`}
+              aria-label="Episode auf Twitter teilen"
+            >
+              <TwitterIcon className={shareIcon.className} />
+            </a>
+          </li>
+          <li>
+            <a
+              href={`mailto:?subject=${sharingText}&body=${currentUrl}`}
+              aria-label="Episode per Email teilen"
+            >
+              <EnvelopeIcon className={shareIcon.className} />
+            </a>
+          </li>
+        </ul>
+      </Constraint>
+    </footer>
+  );
+};
